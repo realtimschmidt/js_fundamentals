e85b094bf2c7ef06b94b9a6bb90c00ac1a9b6acb
realtimschmidt <schmidt.tim.j@gmail.com>
2019-01-07 22:16:14 -0500

    filter and reduce

---
+++ src/simplify_js_map_reduce_filter/filter.js
@@ -0,0 +1,27 @@
+const pilots = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels",
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire",
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire",
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels",
+  }
+]
+
+const rebel = pilots.filter((pilot) => pilot.faction === 'Rebels')
+const empire = pilots.filter((pilot) => pilot.faction === 'Empire')
+console.log({ rebel })
+console.log({ empire })
