ca4b0eaf722e8c5d95e34b3845eea99e533a6c7e
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-16 11:47:13 -0500

    test for multiple journal entries

--- src/eloquent_javascript/__tests__/ch4_structures_in_chapter_practice.test.js
+++ src/eloquent_javascript/__tests__/ch4_structures_in_chapter_practice.test.js
@@ -1,24 +1,37 @@
 const addToJournal = require('../ch4_structures_in_chapter_practice')
 
 let events
-let wasSqurriel
+let wasSquirrel
+let events2
+let wasSquirrel2
 let result
 
 describe('squirrel journal', () => {
   beforeEach(() => {
     events = ["work", "touched tree", "pizza", "running",
     "television"]
-    wasSqurriel = false
+    wasSquirrel = false
     result = [
       {
         events,
-        squirrel: wasSqurriel
+        squirrel: wasSquirrel
       }
     ]
   })
 
 
   it('adds an enrty to the journal', () => {
-    expect(addToJournal(events, wasSqurriel)).toEqual(result)
+    expect(addToJournal(events, wasSquirrel)).toEqual(result)
+  })
+
+  it('adds multiple entries to the jorunal', () => {
+    events = ["work", "ice cream", "cauliflower", "lasagna",
+    "touched tree", "brushed teeth"]
+    wasSquirrel = true
+    result.push({
+      events,
+      squirrel: wasSquirrel
+    })
+    expect(addToJournal(events, wasSquirrel)).toEqual(result)
   })
 })
