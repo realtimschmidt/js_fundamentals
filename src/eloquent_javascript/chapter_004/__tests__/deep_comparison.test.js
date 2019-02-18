b2de4df5708bc734bbac16ec037816a38bd7b0e8
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-18 00:16:44 -0600

    unit tests for object deep compare

--- src/eloquent_javascript/chapter_004/__tests__/deep_comparison.test.js
+++ src/eloquent_javascript/chapter_004/__tests__/deep_comparison.test.js
@@ -1,14 +1,24 @@
 const deepCompare = require('../deep_comparison')
 
 let number
+let object
 
 describe('deep comparison', () => {
   beforeEach(() => {
     number = 4
+    object = {
+      a: 1,
+      b: 2
+    }
   })
 
   it('compares primitives', () => {
     expect(deepCompare(number, 4)).toEqual(true)
     expect(deepCompare(number, 3)).toEqual(false)
+  })
+
+  it('compares objects', () => {
+    expect(deepCompare(object, object)).toBe(true)
+    expect(deepCompare(object, {a:1, b:2})).toBe(true)
   })
 })
